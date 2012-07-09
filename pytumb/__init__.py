9dbda7922b077d460913f0e7d0b44d63e8bfdc35
PyYoshi <yohihiro.m@gmail.com>
2012-07-09 23:33:15 +0900

    init

---
+++ pytumb/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__vesion__ = ''
+__author__ = 'PyYoshi'
+__license__ = 'MIT License'
+__url__ = 'https://github.com/PyYoshi/Pytumb'
+
+from pytumb.auth import OAuthHandler, BasicAuthHandler
