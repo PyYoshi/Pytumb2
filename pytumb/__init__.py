1f3c85263bb0c90a2c5d204086be6ea8f57a46fc
PyYoshi <yohihiro.m@gmail.com>
2012-07-12 11:38:35 +0900

    completed prototype.

--- pytumb/__init__.py
+++ pytumb/__init__.py
@@ -4,6 +4,8 @@
 __vesion__ = ''
 __author__ = 'PyYoshi'
 __license__ = 'MIT License'
-__url__ = 'https://github.com/PyYoshi/Pytumb'
+__url__ = 'https://github.com/PyYoshi/Pytumb2'
 
-from pytumb.auth import OAuthHandler, BasicAuthHandler
+from pytumb.auth import OAuthHandler
+from pytumb.api import API
+from pytumb.utils import Utils
