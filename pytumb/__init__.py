cd13badcf40dbf6bc95855ba413206f92e9ce4a5
PyYoshi <yohihiro.m@gmail.com>
2012-07-14 21:27:12 +0900

    init 1.0.0 version. added TODOs.

--- pytumb/__init__.py
+++ pytumb/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__vesion__ = ''
+__vesion__ = '1.0.0'
 __author__ = 'PyYoshi'
 __license__ = 'MIT License'
 __url__ = 'https://github.com/PyYoshi/Pytumb2'
@@ -9,3 +9,6 @@ __url__ = 'https://github.com/PyYoshi/Pytumb2'
 from pytumb.auth import OAuthHandler
 from pytumb.api import API
 from pytumb.utils import Utils
+
+# 全体的な TODO: ユーザ側が使用する関数のドキュメントの構築
+# 全体的な TODO: 重複処理の簡素化。新たに変数を生成しなくてもいい処理等。  ※ただし、可読性を悪くする修正は行わないこと。
